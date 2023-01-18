7874dd35be320c135fa159929b50dd44c6dea8cb
=vantruyen05.work@gmail.com <you@example.com>
2023-01-18 15:18:27 +0700

    init project

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Footer from '../components/Footer';
 import PostCard from '../components/PostCard';
 import { getAllPosts } from '../lib/test-data';
+import { client } from "../lib/apollo";
+import { gql } from '@apollo/client';
 
 
 export default function Home({ posts }) {
@@ -39,7 +41,21 @@ export default function Home({ posts }) {
 
 export async function getStaticProps(){
 
-  const response = await getAllPosts()
+  const GET_POST = gql`
+    query GetAllPosts {
+      posts {
+        nodes {
+          title
+          content
+          uri
+          date
+        }
+      }
+    }
+  `
+  const response = await client.query({
+    query: GET_POST
+  })
   const posts = response?.data?.posts?.nodes
   return {
     props: {
